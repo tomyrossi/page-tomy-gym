4980d600f28346c188c1ed97db55f3c94a913f62
tomyrossi <tomasrossi@live.com.ar>
2023-03-08 20:13:19 -0300

    Primer commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,40 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "gray-20": "#EFDEDE",
+        "gray-50": "#EECDCD",
+        "gray-100": "#C8BDBD",
+        "gray-500": "#9C8989",
+        "primary-100": "#3FDD90",
+        "primary-300": "#7BF0B8",
+        "primary-500": "#C4F7DF",
+        "secondary-400": "#FC6363",
+        "secondary-500": "#FA8F8F",
+      },
+      backgroundImage: (theme) => ({
+        "gradient-yellowred":
+          "linear-gradient(90deg, #FF616A 0%, #FFC837 100%)",
+        "mobile-home": "url('./assets/HomePageGraphic.png')",
+      }),
+      fontFamily: {
+        dmsans: ["DM Sans", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+      content: {
+        evolvetext: "url('./assets/EvolveText.png')",
+        abstractwaves: "url('./assets/AbstractWaves.png')",
+        sparkles: "url('./assets/Sparkles.png')",
+        circles: "url('./assets/Circles.png')",
+      },
+    },
+    screens: {
+      xs: "480px",
+      sm: "768px",
+      md: "1060px",
+    },
+  },
+  plugins: [],
+};
